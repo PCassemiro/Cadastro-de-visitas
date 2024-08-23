0ecfd648db7ce0ee04a1e971066ecbb3a5d0390a
PCassemiro <pedrobr2909@gmail.com>
2024-08-23 16:49:04 -0300

    Arquivo atualizado

---
+++ script001.js
@@ -0,0 +1,56 @@
+
+console.log(
+        // window.prompt('ola?')
+        window.document.querySelector('.Chamado')
+        if (select=='não') {
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const dataHoraAtual = new Date();
+// console.log(dataHoraAtual);
+// const dataFormatada = dataHoraAtual.toLocaleDateString(); // Apenas a data
+// const horaFormatada = dataHoraAtual.toLocaleTimeString(); // Apenas a hora
+// console.log(`Data: ${dataFormatada}, Hora: ${horaFormatada}`);
